013c768b1e60b264ac0cb372f565b1fd6c037b83
Koch <matheuskoch13@gmail.com>
2019-05-13 06:03:41 -0300

    Inicial Servidor metodos de envio e busca Blicliotecas que faltaram para executar

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 linux ftp server
+
+
+
+iniciar Servidor
+g++ main.cpp header.h server.c -o server
+./server
+
+
+inicial Cleinte
+
+g++ client.c -o client
+./client
--- src/header.h
+++ src/header.h
@@ -1,4 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+
 
 int start_server();
+char *pegaValor(int new_socket);
+void  enviarValor(int new_socket,char *hello );
